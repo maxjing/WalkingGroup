63e0ad6e4514ca59559d6a17aed51bc3734100c5
maxjing <tvandcc@gmail.com>
2018-06-29 15:32:56 -0700

    add test packages

--- app/src/main/java/ca/cmpt276/walkinggroup/test_wj/MainActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/test_wj/MainActivity.java
@@ -1,13 +1,15 @@
-package ca.cmpt276.walkinggroup.app;
+package ca.cmpt276.walkinggroup.test_wj;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import ca.cmpt276.walkinggroup.app.R;
 
 public class MainActivity extends AppCompatActivity {
     private String token;
