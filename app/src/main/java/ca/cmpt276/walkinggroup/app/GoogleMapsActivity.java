e00aaeb487188ac433adb9c68d5adbde17635d11
Luming_Zhang <zlm1120@outlook.com>
2018-06-29 23:19:38 -0700

    add the place in google map API

--- app/src/main/java/ca/cmpt276/walkinggroup/app/GoogleMapsActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/GoogleMapsActivity.java
@@ -1,7 +1,14 @@
 package ca.cmpt276.walkinggroup.app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,7 +19,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "";
     private GoogleMap mMap;
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMITION_REQUEST_CODE = 1234;
+
+    //vars
+    private Boolean mLocationPermitionsGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +37,65 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        getLocationPermission();
     }
 
+    private void intiMap(){
+        Log.d(TAG, "intiMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+            }
+        });
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermitionsGranted = true;
+                intiMap();
+            }else{
+                ActivityCompat.requestPermissions(this,permissions,
+                        LOCATION_PERMITION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,permissions,
+                    LOCATION_PERMITION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermitionsGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMITION_REQUEST_CODE:{
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                            mLocationPermitionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionResult: permission granted");
+                    mLocationPermitionsGranted = true;
+                    //initialize our map
+                    intiMap();
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -36,6 +108,8 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "OnMapReady: Map is Ready");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
