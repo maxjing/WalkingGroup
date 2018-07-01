806e3d15a423d8e199b5cff9c65cc7adf71a1d61
maxjing <tvandcc@gmail.com>
2018-06-30 21:07:10 -0700

    add userinfo show

--- app/src/main/java/ca/cmpt276/walkinggroup/app/UserinfoActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/UserinfoActivity.java
@@ -2,6 +2,7 @@ package ca.cmpt276.walkinggroup.app;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,43 +20,33 @@ public class UserinfoActivity extends AppCompatActivity {
     private WGServerProxy proxy;
     private static final String TAG = "Userinfo";
     private String userEmail;
+    private String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userinfo);
-
-        proxy = ProxyBuilder.getProxy(getString(R.string.apikey), null);
-
+        SharedPreferences dataToGet = getApplicationContext().getSharedPreferences("userPref",0);
+        token = dataToGet.getString("userToken","");
+        proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
         user = User.getInstance();
-//        Toast.makeText(UserinfoActivity.this, "Email:"+user.getEmail(), Toast.LENGTH_SHORT).show();
-//        userEmail = user.getEmail();
-//        Call<User> caller = proxy.getUserByEmail(userEmail);
-//        ProxyBuilder.callProxy(UserinfoActivity.this, caller, returnedUser -> response(returnedUser));
+
         userEmail = user.getEmail();
-        setupGetUserByEmail();
+        Call<User> caller = proxy.getUserByEmail(userEmail);
+        ProxyBuilder.callProxy(UserinfoActivity.this, caller, returnedUser -> response(returnedUser));
+
+
 
 
 
     }
+
     private void response(User user) {
         notifyUserViaLogAndToast("Server replied with user: " + user.toString());
         userEmail = user.getEmail();
     }
 
-    private void setupGetUserByEmail() {
-        Button btn = findViewById(R.id.btnGetEmail);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Make call
-                Toast.makeText(UserinfoActivity.this, "email from userinfo:"+userEmail, Toast.LENGTH_SHORT).show();
-                Call<User> caller = proxy.getUserByEmail(userEmail);
-                ProxyBuilder.callProxy(UserinfoActivity.this, caller, returnedUser -> response(returnedUser));
 
-            }
-        });
-    }
 
     public static Intent makeIntent(Context context){
         return new Intent(context, UserinfoActivity.class);
