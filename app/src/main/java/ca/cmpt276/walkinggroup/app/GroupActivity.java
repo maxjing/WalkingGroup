20fdc736a5fe97a30a27db1a46689e27030185ed
maxjing <tvandcc@gmail.com>
2018-07-01 23:19:11 -0700

    able to create group

--- app/src/main/java/ca/cmpt276/walkinggroup/app/GroupActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/GroupActivity.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import ca.cmpt276.walkinggroup.dataobjects.EarnedRewards;
+import ca.cmpt276.walkinggroup.dataobjects.Group;
 import ca.cmpt276.walkinggroup.dataobjects.User;
 import ca.cmpt276.walkinggroup.proxy.ProxyBuilder;
 import ca.cmpt276.walkinggroup.proxy.WGServerProxy;
+import retrofit2.Call;
 
 public class GroupActivity extends AppCompatActivity {
     private String token;
@@ -17,16 +22,39 @@ public class GroupActivity extends AppCompatActivity {
     private WGServerProxy proxy;
     private String userEmail;
     private User user;
+    private Group group;
+    private String groupName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
         SharedPreferences dataToGet = getApplicationContext().getSharedPreferences("userPref",0);
         token = dataToGet.getString("userToken","");
-        userEmail = dataToGet.getString("userEmail","");
         proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
-        Toast.makeText(this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
+        setupNewGroupButton();
     }
+
+    private void setupNewGroupButton() {
+        Button btn = findViewById(R.id.btnNewGroup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Build new user (with random email to avoid conflicts)
+                group = new Group();
+                group.setName("wjwjwjwjwj");
+
+                // Make call
+                Call<Group> caller = proxy.createGroup(group);
+                ProxyBuilder.callProxy(GroupActivity.this, caller, returnedGroup -> response(returnedGroup));
+            }
+        });
+    }
+    private void response(Group group) {
+
+        groupName = group.getName();
+        Toast.makeText(GroupActivity.this, ""+groupName, Toast.LENGTH_SHORT).show();
+    }
+
     public static Intent makeIntent(Context context){
         return new Intent(context, GroupActivity.class);
     }
--- app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/Group.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/Group.java
@@ -10,4 +10,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Group extends IdItemBase{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Group{" +
+//                "id=" + getId() +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
