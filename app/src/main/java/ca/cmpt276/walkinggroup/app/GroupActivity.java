c944e9ba3b73a179dc383d97d41b910d14cefd5c
Lei Chen <lca121@sfu.ca>
2018-07-04 00:08:56 -0700

    group.xml

--- app/src/main/java/ca/cmpt276/walkinggroup/app/GroupActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/GroupActivity.java
@@ -6,7 +6,9 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -22,6 +24,8 @@ public class GroupActivity extends AppCompatActivity {
     private String token;
     private String TAG = "GroupActivity";
     private WGServerProxy proxy;
+    private User user;
+    private List<Group> leadsGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +34,44 @@ public class GroupActivity extends AppCompatActivity {
         SharedPreferences dataToGet = getApplicationContext().getSharedPreferences("userPref",0);
         token = dataToGet.getString("userToken","");
         proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
-        setupNewGroupButton();
+        user = User.getInstance();
+        Call<User> caller = proxy.getUserByEmail(user.getEmail());
+        ProxyBuilder.callProxy(GroupActivity.this, caller, returnedUser -> response(returnedUser));
+       // setupNewGroupButton();
         setupTestButton();
 
     }
 
-    private void setupNewGroupButton() {
-        Button btn = findViewById(R.id.btnNewGroup);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentToCreate = GroupCreateActivity.makeIntent(GroupActivity.this);
-                startActivity(intentToCreate);
-            }
-        });
+    private void response(User returnedUser) {
+       // leadsGroups = returnedUser.getLeadsGroups();
+        String msg="";
+        String[] items = new String[returnedUser.getLeadsGroups().size()];
+        for (int i = 0; i < returnedUser.getLeadsGroups().size(); i++) {
+            items[i] = returnedUser.getLeadsGroups().get(i).getId() + " - " + returnedUser.getLeadsGroups().get(i).getGroupDescription();
+                    //+" "+leadsGroups.get(i).getLeader().getId();
+            //msg = msg + items[i];
+        }
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.lead, items);
+        ListView list = (ListView) findViewById(R.id.list_leader);
+        list.setAdapter(adapter);
     }
 
+
+//    private void setupNewGroupButton() {
+//        Button btn = findViewById(R.id.btnNewGroup);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentToCreate = GroupCreateActivity.makeIntent(GroupActivity.this);
+//                startActivity(intentToCreate);
+//            }
+//        });
+//    }
+
     private void setupTestButton() {
-        Button btn = findViewById(R.id.button);
+        Button btn = findViewById(R.id.btnTest);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
