266c5a5fe761af2b74f031c2bfcc72094585c010
Luming Zhang <zlm1120@outlook.com>
2018-07-11 12:19:26 -0700

    set GpsLocation

--- app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/GpsLocation.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/GpsLocation.java
@@ -1,6 +1,7 @@
 package ca.cmpt276.walkinggroup.dataobjects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Store information about a GPS location of a user.
@@ -10,4 +11,17 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GpsLocation {
+    private LatLng mLatLng;
+
+    public GpsLocation(LatLng mLatLng) {
+        this.mLatLng = mLatLng;
+    }
+
+    public LatLng getmLatLng() {
+        return mLatLng;
+    }
+
+    public void setmLatLng(LatLng mLatLng) {
+        this.mLatLng = mLatLng;
+    }
 }
