8a9e49bfce988fbd17ddbe9759fee1e6cb3d3f7d
Lei Chen <lca121@sfu.ca>
2018-07-17 20:15:29 -0700

    Fix

--- app/src/main/java/ca/cmpt276/walkinggroup/app/MainActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             btnLogout.setVisibility(View.VISIBLE);
             populate();
-            showChildGPS();
+            //showChildGPS();
         }
 
         setGroupBtn();
--- app/src/main/java/ca/cmpt276/walkinggroup/app/MonitoringActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/MonitoringActivity.java
@@ -159,6 +159,7 @@ public class MonitoringActivity extends AppCompatActivity {
                     token = dataToGet.getString("userToken","");
                     proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
                     user = User.getInstance();
+                    userId = dataToGet.getLong("userId", 0);
                     Call<User> caller = proxy.getUserById(userId);
                     ProxyBuilder.callProxy(MonitoringActivity.this, caller, returnedUser -> response(returnedUser));
                 }
